aeee12ccd8139dbdfc85398cffee0af3c737c53c
Kir93 <kir931028@gmail.com>
2022-07-18 16:56:00 +0900

    update react-query setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,34 @@ import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import theme from '@styles/theme';
 
 import AppLayout from '@components/Layout/AppLayout';
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Head>
-      <title>Tirrilee Next</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
-  </ThemeProvider>
+  <QueryClientProvider client={client}>
+    {process.env.NODE_ENV !== 'production' ? <ReactQueryDevtools initialIsOpen={false} /> : null}
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Next Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ThemeProvider>
+  </QueryClientProvider>
 );
 
 export default App;
