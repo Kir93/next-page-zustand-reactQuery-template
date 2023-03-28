848e5d404b3002a50dd45312c39f9beca0abb6a5
Kir93 <kir931028@gmail.com>
2023-03-28 17:15:18 +0900

    update ts

--- components/Layout/AppLayout.tsx
+++ components/Layout/AppLayout.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { LS } from '.';
 
-const AppLayout = ({ children }: { children: React.ReactElement }): React.ReactElement => (
-  <LS.Container>{children}</LS.Container>
-);
+interface IProps {
+  children: React.ReactElement;
+}
+
+const AppLayout: React.FC<IProps> = ({ children }) => <LS.Container>{children}</LS.Container>;
 
 export default AppLayout;
