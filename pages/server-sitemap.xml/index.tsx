237c0c844ba720fa2048a0774a11755ad5828804
Kir93 <kir931028@gmail.com>
2023-05-04 18:11:04 +0900

    fix ts error

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -1,16 +1,16 @@
 import { GetServerSideProps } from 'next';
 import { getServerSideSitemap } from 'next-sitemap';
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getServerSideProps = (ctx: GetServerSideProps) => {
   const posts: { id: number }[] = [];
   const newsSitemaps = posts.map((id) => ({
-    loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL || 'http://localhost:3000/'}${id.toString()}`,
+    loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL}${id}`,
     lastmod: new Date().toISOString(),
   }));
 
   const fields = [...newsSitemaps];
 
-  return getServerSideSitemap(ctx, fields);
+  return getServerSideSitemap(fields, ctx);
 };
 
 export default function Site() {}
