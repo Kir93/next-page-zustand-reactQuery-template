c4e244e84e24bbdda5f42eaac2888bb9cc2bc873
Kir93 <kir931028@gmail.com>
2023-05-07 18:15:00 +0900

    update fix document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,9 @@ import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
     try {
-      const sheet = new ServerStyleSheet();
       const originalRenderPage = ctx.renderPage;
-
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
