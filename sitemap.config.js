66c84d8a4794dd694c2c94fe93d6e9d8b2cb8ed0
Kir93 <kir931028@gmail.com>
2025-03-16 19:46:53 +0900

    sitemap.config.js 파일 수정: siteUrl 형식 변경 및 추가 사이트맵 경로 수정

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,16 +1,15 @@
-const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL || 'http://localhost:3000/';
+const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL || 'http://localhost:3000';
+
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl,
   exclude: ['/404'],
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
-      {
-        userAgent: '*',
-        disallow: ['/404']
-      },
+      { userAgent: '*', disallow: ['/404'] },
       { userAgent: '*', allow: '/' }
     ],
-    additionalSitemaps: [`${siteUrl}server-sitemap.xml`]
+    additionalSitemaps: [`${siteUrl}/server-sitemap.xml`]
   }
 };
