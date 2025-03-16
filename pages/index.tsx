0354a05928f51aa9bddf1a32dbe046aa571ad04d
Kir93 <kir931028@gmail.com>
2025-03-16 19:52:06 +0900

    update renaming store folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 
-import useStore from '@reducers';
+import { Button, Text } from '@chakra-ui/react';
+
+import useStore from 'store';
 
 import CustomColorBar from '@atoms/CustomColorBar/CustomColorBar';
 
 import CommonBar from '@components/Common/CommonBar';
 import { HomeButtonSection } from '@components/Home';
-import { Button, Text } from '@chakra-ui/react';
 
 const Home: FC = () => {
   const me = useStore((state) => state.me);
