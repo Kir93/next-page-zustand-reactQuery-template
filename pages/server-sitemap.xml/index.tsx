916d45d22e219e4382667a41ce4d1eca3b65774d
Kir93 <kir931028@gmail.com>
2025-07-06 12:57:08 +0900

    fix next-sitemap 업데이트 적용

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -1,16 +1,20 @@
 import { GetServerSideProps } from 'next';
-import { getServerSideSitemap } from 'next-sitemap';
+import { getServerSideSitemapLegacy } from 'next-sitemap';
 
-export const getServerSideProps = (ctx: GetServerSideProps) => {
+/*
+ * Boilerplate code for dynamic sitemap generation.
+ * This page is not actually used, but demonstrates how to generate dynamic sitemaps.
+ */
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const posts: { id: number }[] = [];
   const newsSitemaps = posts.map((id) => ({
-    loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL}${id}`,
-    lastmod: new Date().toISOString(),
+    loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL || 'http://localhost:3000/'}${id.toString()}`,
+    lastmod: new Date().toISOString()
   }));
 
   const fields = [...newsSitemaps];
 
-  return getServerSideSitemap(fields, ctx);
+  return getServerSideSitemapLegacy(ctx, fields);
 };
 
 export default function Site() {}
